a1d72bb20d20bf7b8013793ed8ecbdd082366a20
gooddle <vptl6559@gmail.com>
2024-06-19 19:06:03 +0900

    :tada: start project

---
+++ src/main/kotlin/com/teamsparta/myblog/MyblogApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.myblog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyblogApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyblogApplication>(*args)
+}
