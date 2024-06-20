9281dcccfdb475b79ad17a28d394378c9493f2ae
gooddle <vptl6559@gmail.com>
2024-06-20 18:55:11 +0900

    :zap: 어노테이션 추가

--- src/main/kotlin/com/teamsparta/myblog/MyblogApplication.kt
+++ src/main/kotlin/com/teamsparta/myblog/MyblogApplication.kt
@@ -2,8 +2,10 @@ package com.teamsparta.myblog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class MyblogApplication
 
 fun main(args: Array<String>) {
