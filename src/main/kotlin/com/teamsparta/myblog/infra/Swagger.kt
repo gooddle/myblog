fb7a925717dcc280d28124664e64e1e53c786ee2
gooddle <vptl6559@gmail.com>
2024-07-01 20:48:51 +0900

    :zap: 어노테이션 수정

--- src/main/kotlin/com/teamsparta/myblog/infra/Swagger.kt
+++ src/main/kotlin/com/teamsparta/myblog/infra/Swagger.kt
@@ -7,8 +7,10 @@ import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @Configuration
+@EnableAspectJAutoProxy
 class SwaggerConfig {
 
     @Bean
