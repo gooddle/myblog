3e1d2764e8589f61f47698e47c1978b5c5be527d
gooddle <vptl6559@gmail.com>
2024-10-07 00:15:29 +0900

    :sparkles: add errorHandler

--- src/main/kotlin/com/teamsparta/myblog/domain/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/teamsparta/myblog/domain/exception/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.teamsparta.myblog.domain.exception
 import com.teamsparta.myblog.domain.exception.dto.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -16,6 +17,13 @@ class GlobalExceptionHandler {
     @ExceptionHandler(IllegalStateException::class)
     fun handlerIllegalStateException(e: IllegalStateException): ResponseEntity<ErrorResponse> {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ErrorResponse(e.message))
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValid(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        val fieldError = e.fieldError
+        val message = "${fieldError?.field}의 입력 값 [${fieldError?.rejectedValue}] 이 유효하지 않습니다"
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse(message))
     }
 
 }
