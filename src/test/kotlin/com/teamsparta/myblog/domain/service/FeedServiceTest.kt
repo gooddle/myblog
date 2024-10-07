962070159a4469792a46865a4ace1ead5376d137
gooddle <vptl6559@gmail.com>
2024-10-07 12:25:06 +0900

    :recycle: refactor test code

--- src/test/kotlin/com/teamsparta/myblog/domain/service/FeedServiceTest.kt
+++ src/test/kotlin/com/teamsparta/myblog/domain/service/FeedServiceTest.kt
@@ -1,41 +1,35 @@
 package com.teamsparta.myblog.domain.service
 
 
-import com.teamsparta.myblog.domain.exception.ModelNotFoundException
 import com.teamsparta.myblog.domain.feed.repository.FeedRepository
 import com.teamsparta.myblog.domain.feed.service.FeedServiceImpl
 import com.teamsparta.myblog.domain.user.repository.UserRepository
+import com.teamsparta.myblog.infra.aop.NotFoundException
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.every
 import io.mockk.mockk
 
 
-class FeedServiceTest:BehaviorSpec({
-
+class FeedServiceTest : BehaviorSpec({
 
     val feedRepository = mockk<FeedRepository>()
     val userRepository = mockk<UserRepository>()
-
     val feedService = FeedServiceImpl(feedRepository, userRepository)
 
 
 
-    Given("특정 feed가 삭제되었을때 "){
-        When("특정 feed를 조회하면 "){
-            Then("ModelNotFoundException 발생해야한다."){
-                val feedId = 30L
-                every { feedRepository.findByFeedIdWithComments(feedId)!!.deleted } returns true
+    Given("특정 feed가 true인 상태 ") {
+        val feedId = 1L
 
-                shouldThrow<ModelNotFoundException> {
-                    feedService.getFeedById(feedId)
-                }
+        When("특정 feed를 조회하면") {
+            every { feedRepository.findByFeedIdWithComments(feedId) } returns null
+
+            Then("feed의 정보가 반환되어야 한다.") {
+             shouldThrow<NotFoundException> {
+                 feedService.getFeedById(feedId)
+             }
             }
         }
     }
-
-
-
-
 })
-
